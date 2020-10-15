9a41943e4f87284f3f0f66dd1a50e566811ebbe2
FLORENCIA CARTONI <fcartoni@uc.cl>
2020-10-15 11:35:25 -0300

    flo jueves am

--- cheffer/src/assets/js/assets.js
+++ cheffer/src/assets/js/assets.js
@@ -2,3 +2,15 @@ import '../images/logo.png';
 import '../images/background.jpg';
 import '../images/cheffer-inicio.png';
 import '../images/bandera-lateral.png';
+import '../images/activity.png';
+import '../images/explore.JPG';
+import '../images/messages.png';
+import '../images/upload.png';
+import '../images/lupa.png';
+import '../images/mamapapa.JPG';
+import '../images/saved.png';
+import '../images/tagged.png';
+import '../images/posts.png';
+import '../images/no-pic.png';
+
+
