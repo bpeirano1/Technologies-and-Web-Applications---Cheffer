62f9b6c88306c85b9b89897edb7c2edc3ac0f346
Bartolome <bpeirano1@uc.cl>
2020-11-28 18:57:49 -0300

    Subida para trabjar con la flo

--- cheffer/src/routes/api/api-rest.js
+++ cheffer/src/routes/api/api-rest.js
@@ -12,7 +12,7 @@ router.get("api.show", "/", async (ctx) => {
     const options = {
         method: 'GET',
         url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer',
-        params: {q: 'How much vitamin c is in 2 apples?'},
+        params: {q: 'How much calories are in 2 risotto?'},
         headers: {
         'x-rapidapi-key': 'f643b8fc06msh3ce76efe81c8531p18b404jsn812065b87303',
         'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
